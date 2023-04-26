99e795c558ee702e02086bcb4dc928e0530ee4b6
chenjiyang0220 <131650732+chenjiyang0220@users.noreply.github.com>
2023-04-26 11:04:11 +0800

    Update doubletfinder.R

--- code/QC/doubletfinder.R
+++ code/QC/doubletfinder.R
@@ -15,24 +15,9 @@ combined$index=gsub('col','',combined$index)
 sample=as.character(unique(Idents(combined)))
 sample=grep('WT1|WT2|OB1|OB2',sample,value = T)
 
-batch1=subset(combined,idents=sample)
-
-I=which(batch1$index %in% c(1:4,13:16,41:44))
-batch1$plate =0
-batch1$plate[I]=paste('I')
-II=which(batch1$index %in% c(65:76))
-batch1$plate[II]=paste('II')
-III=which(batch1$index %in% c(77:88))
-batch1$plate[III]=paste('III')
-IV=which(batch1$index %in% c(89:96))
-batch1$plate[IV]=paste('IV')
-Idents(batch1) <- batch1$plate
-type=as.character(unique(Idents(batch1)))
-type
-#type <- as.character(unique(Idents(combined)))
-#type <- grep('OB9',type,value = T)
-#type <- type[type %ni% 'WT4_white_fat']
-#table(combined$tissue,combined$sample)
+type <- as.character(unique(Idents(combined)))
+type <- grep('OB9',type,value = T)
+table(combined$tissue,combined$sample)
 for(i in 1:length(type)){
   temp <- subset(batch1,idents=type[i])
   temp <- GetAssayData(temp@assays$RNA,slot="counts")
